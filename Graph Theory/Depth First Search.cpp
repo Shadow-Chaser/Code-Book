f6060b11d8f6b05c11c397a92fdc390c4063e0eb
Ashab Hussan <46869907+Shadow-Chaser@users.noreply.github.com>
2020-11-07 23:07:02 +0600

    Create Depth First Search.cpp

---
+++ Graph Theory/Depth First Search.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+#define MX 100
+
+vector <int> g[MX];
+bool vis[MX];
+
+
+void dfs(int source) {
+	vis[source] = 1;
+	for (int i = 0; i < g[source].size(); i++) {
+		int next = g[source][i];
+		if (vis[next] == 0)
+			dfs(next);
+	}
+}
+
+
+int main()
+{
+
+	int nodes, edges;
+	cin >> nodes >> edges;
+
+	for (int i = 1; i <= edges; i++)
+	{
+		int u, v;
+		cin >> u >> v;
+		g[u].push_back(v);
+		g[v].push_back(u);
+	}
+
+	int s;
+	cin >> s;
+	dfs(s);
+
+	for (int i = 1; i <= nodes; i++) {
+		if (vis[i] == 1) {
+			cout << "Node " << i << " is visited." << endl;
+
+		}
+		else {
+			cout << "Node " << i << " is not visited" << endl;
+		}
+	}
+
+	return 0;
+}
